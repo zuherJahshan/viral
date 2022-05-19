e982ef07ac09b4a64acfa7c6e1300e3bd4659988
zuher1711 <zuher1711@gmail.com>
2022-05-19 19:33:01 +0300

    skeleton of application

--- src/CoViT.py
+++ src/CoViT.py
@@ -8,9 +8,101 @@ from Model import CoViTModel
 
 class HyperParameters(object):
     def __init__(self):
-        self.encoder_repeats: int = 8   # Number of times the encoder block is repeated
-        self.d_out: int = 2             # Number of classes from which we should classify
-        self.d_model: int               # the dimensionality of the feature vectors also equals fragment_length
+        """
+        These hyperparameter controls the neural network model. The default definition of those parameters are taken
+        from the paper Attention is all you need.
+        """
+        self._encoder_repeats: int = 8   # Number of times the encoder block is repeated
+        self._d_out: int = 2             # Number of classes from which we should classify
+        self._d_model: int = 256         # The dimensionality of the feature vectors also equals fragment_length
+        self._d_val: int = 64            # The dimensionality of the value representation of a fragment (for self attention)
+        self._d_key: int = 64            # The dimensionality of the key representation of a fragment (for self attention)
+        self._heads: int = 8             # Number of heads used in the self attention layer.
+        self._d_ff: int = 2048           # Feed forward hidden layer inner number of units
+
+        self._kmer_size: int = 30        # Anchor kmer size. This hyperparameter controls the type of genome encoding
+        self._n: int = 256               # Compression factor, controls the information extracted from genome to the encoding
+
+    @property
+    def encoder_repeats(self):
+        return self._encoder_repeats
+
+    @encoder_repeats.setter
+    def encoder_repeats(self,
+                        encoder_repeats: int):
+        self._encoder_repeats = encoder_repeats
+
+    @property
+    def d_out(self):
+        return self._encoder_repeats
+
+    @d_out.setter
+    def d_out(self,
+              d_out: int):
+        self._d_out = d_out
+
+    @property
+    def d_model(self):
+        return self._d_model
+
+    @d_model.setter
+    def d_model(self,
+                d_model):
+        self._d_model = d_model
+
+    @property
+    def d_val(self):
+        return self._d_val
+
+    @d_val.setter
+    def d_val(self,
+              d_val):
+        self._d_val = d_val
+
+    @property
+    def d_key(self):
+        return self._d_key
+
+    @d_key.setter
+    def d_key(self,
+              d_key):
+        self._d_key = d_key
+
+    @property
+    def heads(self):
+        return self._heads
+
+    @heads.setter
+    def heads(self,
+              heads):
+        self._heads = heads
+
+    @property
+    def d_ff(self):
+        return self._d_ff
+
+    @d_ff.setter
+    def d_ff(self,
+             d_ff):
+        self._d_ff = d_ff
+
+    @property
+    def kmer_size(self):
+        return self._kmer_size
+
+    @kmer_size.setter
+    def kmer_size(self,
+                  kmer_size):
+        self._kmer_size = kmer_size
+
+    @property
+    def n(self):
+        return self._n
+
+    @n.setter
+    def n(self,
+          n):
+        self._n = n
 
 
 class CoViT(object):
@@ -24,11 +116,118 @@ class CoViT(object):
     """
     def __init__(self):
         # Set the DataCollector
-        self.data_collector = DataCollector()
+        self.data_collector: DataCollector = DataCollector()
 
         # Set the Accessions dataframe which holds all metadata
-        self.acc_df = self.data_collector.getAccDF()
+        self.acc_df: pd.DataFrame = self.data_collector.getAccDF()
+
+        # Set hyper parameters
+        self.HP: HyperParameters = HyperParameters()
 
         # Set model
+        self.nn_model: tf.keras.Model = None
+
+        # Set Dataset
+        self.dataset: tf.data.Dataset = None
+
+    def train(self):
+        """
+        1. choose fasta files for train set
+        2. choose fasta files for valid set
+        3. choose fasta files for test set - (optional)
+        how to choose?
+            a. The list of virus classes should be given first
+            b. the size of dataset to build tiny/small/medium/big/huge
+        """
+        return
+
+    def evaluate(self):
+        """
+        Just evaluate the results on the training set, on the validation set.
+        """
+        return
+
+    def loadModel(self,
+                  saved_model_path: str):
+        """
+        given a saved model, just load it in. For a reference see how it is done in the TestModel file
+        """
+        return
+
+    def _updateNNModel(self):
+        """
+        There is some hyper parameters that if changed, the model should be forsaken and a new one
+        """
+        return
+
+    def saveNNModel(self,
+                    model_name):
+        """
+        Saves the model, maybe a model name should not be given,
+        else only saves the model according to HP and date.
+        Models:
+        --> 8-256-64-64-8-2048-30-256 (Hyper parameters)
+            --> set { lineage_set <--> lineage_set_id }
+            --> lineage1
+                --> date1
+                --> date2
+                --> date3
+            --> lineage2
+                --> date1
+                --> date2
+        """
+        return
+
+    def setHP(self,
+              encoder_repeats: int = None,
+              d_out: int = None,
+              d_model: int = None,
+              d_val: int = None,
+              d_key: int = None,
+              heads: int = None,
+              d_ff: int = None,
+              kmer_size: int = None,
+              n: int = None,
+              transfer_model: bool = True):
+
+        to_be_updated: bool = False
+        if encoder_repeats is not None:
+            self.HP.encoder_repeats = encoder_repeats
+            to_be_updated = True
+
+        if d_out is not None:
+            self.HP.d_out = d_out
+            to_be_updated = True
+
+        if d_model is not None:
+            self.HP.d_model = d_model
+            to_be_updated = True
+
+        if d_val is not None:
+            self.HP.d_val = d_val
+            to_be_updated = True
+
+        if d_key is not None:
+            self.HP.d_key = d_key
+            to_be_updated = True
+
+        if heads is not None:
+            self.HP.heads = heads
+            to_be_updated = True
+
+        if d_ff is not None:
+            self.HP.d_ff = d_ff
+            to_be_updated = True
+
+        if kmer_size is not None:
+            self.HP.kmer_size = kmer_size
+
+        if n is not None:
+            self.HP.n = n
+
+        if to_be_updated or not transfer_model:
+            self._updateNNModel()
+        return
+
 
 
